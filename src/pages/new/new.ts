0de26fabcea56f0b075371ee5ac05fc8ad0984f8
Jovid Xamrokulov <jxamrokulov@mail.ru>
2018-05-11 10:14:30 +0500

    New Infitinescroll
    by

--- src/pages/new/new.ts
+++ src/pages/new/new.ts
@@ -19,10 +19,17 @@ export class NewPage {
   created_date : any [];
   image : any [];
   id : any [];
- 
+  
+  realNews : any ;
+  items = [];
+  
+
   constructor(public navCtrl: NavController, public navParams: NavParams,public getdata : GetdataProvider) {
     this.getNews();
-  
+    for (let i = 0; i < 30; i++) {
+      this.items.push( this.items.length );
+    }
+    
  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewPage');
@@ -38,6 +45,24 @@ export class NewPage {
       this.news = data;
       console.log(this.news);
     });
+  }
+
+  doInfinite(infiniteScroll) {
+  
+    setTimeout(() => {
+      this.getdata.getNews()
+      .then(data => {
+        this.news = data;
+        //console.log(this.news);
+        for(let i=0; i<this.news.length; i++) {
+          this.realNews.push(this.news[i]);
+        }
+
+      });
+         
+     
+      infiniteScroll.complete();
+    }, 500);
   }
 
   
